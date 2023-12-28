2be0702b47f817d343ddd9d0e291f6c01d054b64
vipulddalal <98317422+vipulddalal@users.noreply.github.com>
2023-12-28 08:16:17 +0530

    Create t2.py

---
+++ t2.py
@@ -0,0 +1,7 @@
+# Task: Create a function that reverses the words in a given sentence
+
+def reverse_words(sentence):
+    words = sentence.split()
+    # Use Copilot to complete the code to reverse the words in the sentence
+    reversed_sentence = ' '.join(reversed(words))
+    # TODO: Return the reversed sentence
