9dee32626b836b644c315de02311575c446a37f1
vipulddalal <98317422+vipulddalal@users.noreply.github.com>
2023-12-28 08:16:45 +0530

    Create t3.py

---
+++ t3.py
@@ -0,0 +1,8 @@
+import requests
+
+# Task: Make a GET request to 'https://api.example.com/data' and print the response JSON
+
+url = 'https://api.example.com/data'
+# Use Copilot to complete the code to make the GET request and print the response JSON
+response = requests.get(url)
+# TODO: Print the response JSON
