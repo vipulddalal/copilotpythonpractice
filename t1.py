c317962b5d43b975905fc20e526145447a51fc78
vipulddalal <98317422+vipulddalal@users.noreply.github.com>
2023-12-28 08:15:16 +0530

    Create t1.py

---
+++ t1.py
@@ -0,0 +1,6 @@
+# Task: Read the contents of 'data.txt' and print each line with line numbers
+
+with open('data.txt', 'r') as file:
+    # Use Copilot to complete the code to read and print each line with line numbers
+    for line in file:
+        # TODO: Complete the code here
